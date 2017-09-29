63b802b85cda8e1b541840f09dd83d2086c1d848
Maciek <maciej.michal.barnas@gmail.com>
2017-09-29 17:31:17 +0200

    modificate file m

--- plot_m.py
+++ plot_m.py
@@ -11,49 +11,28 @@ matplotlib.style.use('classic')
 
 # Arguments to pass:
 filename = 'Razem_Laboratorium.csv'
-# c_prof = 5  # Number of column, in filename, with well log data
-# c_lab = 8  # Number of column, in filename, with laboratory data
-c_prof = 'PorowatoscCalkowitaPiknometria'  # Name of column, in filename, with well log data
+c_prof = 'WspPorCalkowitejNMR'  # Name of column, in filename, with well log data
+unit_c_prof = '%'
 c_lab = 'ParametrPorowatosci'  # Name of column, in filename, with laboratory data
-x_axis_type = 'linear'  # Choose x axis scale - linear or log
+unit_c_lab = '-'
+x_axis_type = 'log'  # Choose x axis scale - linear or log
 y_axis_type = 'log'  # Choose y axis scale - linear or log
 fig_size_x = 24
 fig_size_y = 12
 size_font = 12
-rys_nr_x = 0.2
-rys_nr_y = 16.5
+rys_nr_x = 1.1
+rys_nr_y = 6000
 empty_value = -999.25
 
 variables = np.genfromtxt(fname=filename, dtype=str, delimiter=';', unpack=True, max_rows=1)
-units = np.genfromtxt(fname=filename, dtype=str, skip_header=1, delimiter=';', unpack=True, max_rows=1)
 data = pd.read_csv(filename, skiprows=2, delimiter=';', names=variables)
 data = data[['Otwor', 'DEPTH', 'Litostratygrafia', c_prof, c_lab]]
 data = data[(data[c_prof] != empty_value) & (data[c_lab] != empty_value)]  # Drop all rows with empty value
+#data = data[data[c_prof] > 1]
 print(data)
 data_lit = data.groupby('Litostratygrafia')
 data_well = data.groupby('Otwor')
-'''
-for group, frame in data_gr:
-    print(group)
-    print(frame)
-'''
-#print(type(data['PorowatoscCalkowitaPiknometria'].iloc[5]))
-# # Calculations:
-# variables = np.genfromtxt(fname=filename, dtype=str, delimiter=';', unpack=True, max_rows=1)
-# units = np.genfromtxt(fname=filename, dtype=str, skip_header=1, delimiter=';', unpack=True, max_rows=1)
-# data = np.genfromtxt(fname=filename, dtype=str, skip_header=2, delimiter=';', unpack=True)
-#
-# data2 = {}
-# for i in range(len(data)):
-#     temp = data[i]
-#     try:
-#         temp = temp.astype(float)
-#     except ValueError:
-#         print('')
-#     data2[variables[i]] = temp
-#
-# print(data2)
-#
+
 def cm2inch(*centy):  # Converting centimeters to inches
     inch = 2.54
     if isinstance(centy[0], tuple):
@@ -64,144 +43,142 @@ def cm2inch(*centy):  # Converting centimeters to inches
 matplotlib.rc('xtick', labelsize=size_font)
 matplotlib.rc('ytick', labelsize=size_font)
 
-#f = plt.figure(figsize=cm2inch(fig_size_x, fig_size_y))  # Size of the figure in centimeters
-#ax = fig.add_subplot(111)
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=cm2inch(fig_size_x, fig_size_y))
 point_size = 60
 
+# for group, frame in data_lit:
+#     print(group)
+#     print(frame)
+
+colors_well = {'A-1': 'r', 'A-2': 'dodgerblue', 'A-3': 'g', 'A-4': 'k'}
+markers_well = {'A-1': 'o', 'A-2': '^', 'A-3': '+', 'A-4': 'x'}
+for group, frame in data_well:
+    ax1.scatter(data_well[c_prof].get_group(group), data_well[c_lab].get_group(group), c=colors_well[group],
+                label=group, s=point_size, marker=markers_well[group])
+
+ax1.text(rys_nr_x, rys_nr_y, 'a)', fontsize=size_font+8)
+ax2.text(rys_nr_x, rys_nr_y, 'b)', fontsize=size_font+8)
+handles, labels = ax1.get_legend_handles_labels()
+by_label = OrderedDict(zip(labels, handles))
+ax1.legend(by_label.values(), by_label.keys(), scatterpoints=1, loc=1, fancybox=True, shadow=True, ncol=2,
+          fontsize=size_font)
+ax1.set_xlabel(c_prof + ' [' + unit_c_prof + ']', fontsize=size_font)
+ax1.set_ylabel(c_lab + ' [' + unit_c_lab + ']', fontsize=size_font)
+ax1.set_xscale(x_axis_type)
+ax1.set_yscale(y_axis_type)
+ax1.set_axisbelow(True)
+ax1.grid()
+ax1.set_xlim(10 ** 0, 10 ** 2)
+ax1.set_ylim(10 ** 1, 10 ** 4)
+
+# Calculating linear regression
+if x_axis_type == 'linear' and y_axis_type == 'linear':
+    slope, intercept, r_value, p_value, std_err = stats.linregress(data[c_prof], data[c_lab])
+elif x_axis_type == 'log' and y_axis_type == 'log':
+    slope, intercept, r_value, p_value, std_err = stats.linregress(np.log(data[c_prof]), np.log(data[c_lab]))
+elif x_axis_type == 'linear' and y_axis_type == 'log':
+    slope, intercept, r_value, p_value, std_err = stats.linregress(np.log(data[c_prof]), np.log(data[c_lab]))
+
+e_intercept = np.exp(intercept)
+print('slope: ', slope)
+print('intercept: ', intercept)
+print('e_intercept', e_intercept)
+print('r_value: ', r_value)
+#
+x_regr = np.linspace(0.8 * np.min(data[c_prof]), 1.4 * np.max(data[c_prof]))
+y_regr = e_intercept * x_regr ** slope
+
+R2_text = r'$P_p = %.1f$' % e_intercept + r'$p^{%.2f}$' % slope + '\n' + r'$R^2 = %.2f$' %abs(r_value) + '\n' + \
+          r'$m = %.2f$' % abs(slope)
+ax1.text(20, 110, R2_text, color='m', fontsize=size_font+1)
+ax1.plot(x_regr, y_regr, 'm--')
+
+colors_lit = {'C1': 'g', 'J2': 'k', 'J3': 'dodgerblue', 'Ng': 'brown', 'T-P': 'r'}
+markers_lit = {'C1': 'o', 'J2': '^', 'J3': '+', 'Ng': 'x', 'T-P': 'd'}
 for group, frame in data_lit:
-    print(group)
-    print(frame)
+    ax2.scatter(data_lit[c_prof].get_group(group), data_lit[c_lab].get_group(group), c=colors_lit[group],
+                label=group, s=point_size, marker=markers_lit[group])
 
-#for i in data_lit[c_prof].get_group('Ng'):
+handles, labels = ax2.get_legend_handles_labels()
+by_label = OrderedDict(zip(labels, handles))
+ax2.legend(by_label.values(), by_label.keys(), scatterpoints=1, loc='best', fancybox=True, shadow=True, ncol=3,
+           fontsize=size_font)
 
-# for i in range(len(data2[variables[0]])):
-#     if data2[variables[c_prof]][i] != -999.25 and data2[variables[c_lab]][i] != -999.25:
-#         if data2[variables[0]][i] == 'L-1':
-#             ax1.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='r', label='A-1', s=point_size)
-#         elif data2[variables[0]][i] == 'L-2':
-#             ax1.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='dodgerblue', label='A-2',
-#                        s=point_size, marker='^')
-#         elif data2[variables[0]][i] == 'L-3':
-#             ax1.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='g', label='A-3', s=point_size,
-#                        marker='+')
-#         elif data2[variables[0]][i] == 'L-4':
-#             ax1.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='k', label='A-4', s=point_size,
-#                        marker='x')
-#
-# # Calculating linear regression
-# data3 = []
-# data4 = []
-# if filename == 'Cross_U.csv':
-#     for i in range(len(data2[variables[0]])):
-#         if data2[variables[c_prof]][i] < 4:
-#             if data2[variables[c_prof]][i] != -999.25 and data2[variables[c_lab]][i] != -999.25:
-#                 data3.append(data2[variables[c_prof]][i])
-#                 data4.append(data2[variables[c_lab]][i])
-#                 print(data3[-1], data4[-1])
-# else:
-#     for i in range(len(data2[variables[0]])):
-#         print(i)
-#         if data2[variables[c_prof]][i] != -999.25 and data2[variables[c_lab]][i] != -999.25:
-#             data3.append(data2[variables[c_prof]][i])
-#             data4.append(data2[variables[c_lab]][i])
-#
-# if x_axis_type == 'linear' and y_axis_type == 'linear':
-#     slope, intercept, r_value, p_value, std_err = stats.linregress(data3, data4)
-# elif x_axis_type == 'log' and y_axis_type == 'log':
-#     slope, intercept, r_value, p_value, std_err = stats.linregress(np.log10(data3), np.log10(data4))
-# elif x_axis_type == 'linear' and y_axis_type == 'log':
-#     slope, intercept, r_value, p_value, std_err = stats.linregress(data3, data4)
-#
-# print('slope: ', slope)
-# print('intercept: ', intercept)
-# print('r_value: ', r_value)
-#
-# x_regr = np.linspace(np.min(data3), np.max(data3))
-# y_regr = slope * x_regr + intercept
-# #y_regr = 1074.6 * x_regr ** (-1.17)
-#
-#
-# R2_text = r'y = 0.83x + 1.06''\n'r'$R^2 = $' + str('%.3f' % r_value**2)
-# #R2_text = r'$P_p = 1074.6p^{-1.17}$''\n'r'$R^2 = 0.69$''\n'r'$m = 1.17$'
-# ax1.plot(x_regr, y_regr, 'm--')
-# if x_axis_type == 'linear' and y_axis_type == 'linear':
-#     ax1.text(x_regr[-17], y_regr[-26] + 0.05 * y_regr[-5], R2_text, color='m',
-#             fontsize=size_font)
-# elif x_axis_type == 'log' and y_axis_type == 'log':
-#     ax1.text(x_regr[-2], y_regr[-40] + 0.05 * y_regr[-40], R2_text, color='m',
-#             fontsize=size_font)
-# elif x_axis_type == 'linear' and y_axis_type == 'log':
-#     ax1.text(x_regr[-25], y_regr[-5] + 0.05 * y_regr[-5], R2_text, color='m',
-#             fontsize=size_font)
-#
-# ax1.text(rys_nr_x, rys_nr_y, 'a)', fontsize=size_font+8)
-# ax2.text(rys_nr_x, rys_nr_y, 'b)', fontsize=size_font+8)
-# handles, labels = ax1.get_legend_handles_labels()
-# by_label = OrderedDict(zip(labels, handles))
-# ax1.legend(by_label.values(), by_label.keys(), scatterpoints=1, loc=2, fancybox=True, shadow=True, ncol=2,
-#           fontsize=size_font)
-# ax1.set_xlabel(variables[c_prof] + ' [' + units[c_prof] + ']', fontsize=size_font)
-# ax1.set_ylabel(variables[c_lab] + ' Lab [' + units[c_lab] + ']', fontsize=size_font)
-# ax1.set_xscale(x_axis_type)
-# ax1.set_yscale(y_axis_type)
-# ax1.set_axisbelow(True)
-# ax1.grid()
-# plt.tight_layout()
-# ax1.set_xlim(-0.5, 22)
-# ax1.set_ylim(-0.5, 22)
-# #ax1.set_xlim(1, 100)
-# #ax1.set_ylim(10, 10000)
-# #plt.savefig(filename=variables[c_prof] + '-' + variables[c_lab] + '_Otw.png')
-#
-# #fig = plt.figure(figsize=cm2inch(fig_size_x, fig_size_y))  # Size of the figure in centimeters
-# #ax = fig.add_subplot(111)
-# #point_size = 40
-# for i in range(len(data2[variables[0]])):
-#     if data2[variables[c_prof]][i] != -999.25 and data2[variables[c_lab]][i] != -999.25:
-#         if data2[variables[2]][i] == 'C1':
-#             ax2.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='g', label='C1', s=point_size)
-#         elif data2[variables[2]][i] == 'J2':
-#             ax2.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='k', label='J2', s=point_size,
-#                        marker='^')
-#         elif data2[variables[2]][i] == 'J3':
-#             ax2.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='dodgerblue', label='J3',
-#                        s=point_size, marker='+')
-#         elif data2[variables[2]][i] == 'Ng':
-#             ax2.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='brown', label='Ng', s=point_size,
-#                        marker='x')
-#         elif data2[variables[2]][i] == 'T-P':
-#             ax2.scatter(data2[variables[c_prof]][i], data2[variables[c_lab]][i], c='r', label='T-P', s=point_size,
-#                        marker='d')
-#
-# ax2.plot(x_regr, y_regr, 'm--')
-# if x_axis_type == 'linear' and y_axis_type == 'linear':
-#     ax2.text(x_regr[-17], y_regr[-26] + 0.05 * y_regr[-5], R2_text, color='m',
-#             fontsize=size_font)
-# elif x_axis_type == 'log' and y_axis_type == 'log':
-#     ax2.text(x_regr[-2], y_regr[-40] + 0.05 * y_regr[-40], R2_text, color='m',
-#             fontsize=size_font)
-# elif x_axis_type == 'linear' and y_axis_type == 'log':
-#     ax2.text(x_regr[-25], y_regr[-5] + 0.05 * y_regr[-5], R2_text, color='m',
-#             fontsize=size_font)
-#
-# ax2.text(70, 5000, 'b)', fontsize=size_font+8)
-#
-# handles, labels = ax2.get_legend_handles_labels()
-# by_label = OrderedDict(zip(labels, handles))
-# ax2.legend(by_label.values(), by_label.keys(), scatterpoints=1, loc=2, fancybox=True, shadow=True, ncol=3,
-#           fontsize=size_font)
-# ax2.set_xlabel(variables[c_prof] + ' [' + units[c_prof] + ']', fontsize=size_font)
-# #plt.ylabel(variables[c_lab] + ' Lab [' + units[c_lab] + ']', fontsize=size_font)
-# ax2.set_xscale(x_axis_type)
-# ax2.set_yscale(y_axis_type)
-# ax2.set_axisbelow(True)
-# ax2.grid()
-# plt.tight_layout()
-# ax2.set_xlim(-0.5, 22)
-# ax2.set_ylim(-0.5, 22)
-# #ax2.set_xlim(1, 100)
-# #ax2.set_ylim(10, 10000)
-# #plt.savefig(filename=variables[c_prof] + '-' + variables[c_lab] + '_Lit.png')
-# plt.savefig(filename=variables[c_prof] + '-' + variables[c_lab] + '_razem.png')
-# plt.show()
+ax2.set_xlabel(c_prof + ' [' + unit_c_prof + ']', fontsize=size_font)
+ax2.set_xscale(x_axis_type)
+ax2.set_yscale(y_axis_type)
+ax2.set_axisbelow(True)
+ax2.grid()
+ax2.set_xlim(10 ** 0, 10 ** 2 + 4)
+ax2.set_ylim(10 ** 1, 10 ** 4)
+
+
+# Calculating linear regression
+
+# Define dictionaries:
+slope2 = {}
+intercept2 = {}
+e_intercept2 = {}
+r_value2 = {}
+p_value2 = {}
+std_err2 = {}
+
+# Calculations
+if x_axis_type == 'linear' and y_axis_type == 'linear':
+    for group, frame in data_lit:
+        slope2[group], intercept2[group], r_value2[group], p_value2[group], std_err2[group] = \
+            stats.linregress(data_lit[c_prof].get_group(group), data_lit[c_lab].get_group(group))
+        e_intercept2[group] = np.exp(intercept2[group])
+elif x_axis_type == 'log' and y_axis_type == 'log':
+    for group, frame in data_lit:
+        slope2[group], intercept2[group], r_value2[group], p_value2[group], std_err2[group] = \
+            stats.linregress(np.log(data_lit[c_prof].get_group(group)), np.log(data_lit[c_lab].get_group(group)))
+        e_intercept2[group] = np.exp(intercept2[group])
+elif x_axis_type == 'linear' and y_axis_type == 'log':
+    for group, frame in data_lit:
+        slope2[group], intercept2[group], r_value2[group], p_value2[group], std_err2[group] = \
+            stats.linregress(np.log(data_lit[c_prof].get_group(group)), np.log(data_lit[c_lab].get_group(group)))
+        e_intercept2[group] = np.exp(intercept2[group])
+
+print('slope2: ', slope2)
+print('intercept2: ', intercept2)
+print('e_intercept2: ', e_intercept2)
+print('r_value2: ', r_value2)
+
+R2_text2 = {}
+for group, frame in data_lit:
+    R2_text2[group] = group + ': ' + r'$m = %.2f$' % abs(slope2[group]) + r', $R^2 = %.2f$' % abs(r_value2[group])
+
+text_loc_lit = {'C1': (10, 800), 'J2': (10, 500), 'J3': (10, 300), 'Ng': (1.4, 20), 'T-P': (1.4, 12)}
+for group, frame in data_lit:
+    ax2.text(text_loc_lit[group][0], text_loc_lit[group][1], R2_text2[group], color=colors_lit[group],
+             fontsize=size_font+1)
+
+for group, frame in data_lit:
+    #x_regr2 = [0.8 * np.min(data_lit[c_prof].get_group(group)), 1.4 * np.max(data_lit[c_prof].get_group(group))]
+    x_regr2 = np.linspace(0.8 * np.min(data_lit[c_prof].get_group(group)),
+                          1.4 * np.max(data_lit[c_prof].get_group(group)))
+    y_regr2 = e_intercept2[group] * x_regr2 ** slope2[group]
+    ax2.plot(x_regr2, y_regr2, color=colors_lit[group], linestyle='--')
+
+plt.tight_layout()
+plt.savefig(filename=c_prof + '-' + c_lab + '_m.png')
+plt.show()
+
+# writer = pd.ExcelWriter('output.xlsx')
+# data.to_excel(writer,'Sheet1')
+# writer.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
